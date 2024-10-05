f2807a324e53349e989a83adf27bca65536bd6f1
atharav-bhatnagar-qb <atharav.bhatnagar@quadbtech.com>
2024-10-05 13:53:16 +0530

    hero section structure created

---
+++ src/StarDustAdventures_frontend/src/pages/Landing.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../components/landing/landing.css'
+import Hero from '../components/landing/hero/Hero'
+
+const Landing = () => {
+  return (
+    <div className='page'>
+        <Hero/>
+    </div>
+  )
+}
+
+export default Landing
