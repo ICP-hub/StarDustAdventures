5925962ebbc2128b610a24ef83c3c9bd83e2a60c
ArjunQBTech <arjun.sharma@quadbtech.com>
2024-10-05 17:38:41 +0530

    Update package.json and package-lock.json dependencies order

--- src/StarDustAdventures_frontend/tailwind.config.js
+++ src/StarDustAdventures_frontend/tailwind.config.js
@@ -7,7 +7,13 @@ export default {
   theme: {
     extend: {
       colors:{
-        btnLight:'rgb(123,0,255)'
+        btnLight:'rgb(123,0,255)',
+        secondary : 'rgba(225, 24, 51, 1)',
+        'transparent-white':'rgba(255, 255, 255, 0.41)'
+      },
+      fontFamily:{
+        'silkscreen': ['Silkscreen', 'cursive'],
+        'pixelify-sans': ['Pixelify Sans', 'sans-serif']
       }
     },
   },
