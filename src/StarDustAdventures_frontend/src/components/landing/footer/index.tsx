592b6a1c58a4cc4af89389ef10c31b32ce427acd
ArjunQBTech <arjun.sharma@quadbtech.com>
2024-10-07 15:06:54 +0530

    Added Footer Section Structure. Styling left

---
+++ src/StarDustAdventures_frontend/src/components/landing/footer/index.tsx
@@ -0,0 +1,46 @@
+/*
+    * This file contains the footer component for the landing page.
+    * The footer contains the social media icons.
+    * The social media icons are stored in the SOCIAL_ICONS {@code Array}.
+    * The icons are displayed using the map function.
+    * The type of SOCIAL_ICONS is defined as Socials.
+    * @author @ArjunQBTech
+    * @version 1.0
+*/
+import React from 'react';
+import { FaXTwitter, FaDiscord, FaTelegram  } from "react-icons/fa6";
+import './index.css';
+
+
+type Socials = {
+    name: string;
+    icons: React.FC;
+}
+
+const SOCIAL_ICONS = [
+    {name:'discord', icons:FaDiscord},
+    {name:'telegram', icons:FaTelegram},
+    {name:'twitter', icons:FaXTwitter},
+] as Socials[]
+
+
+const Footer = ()=>{
+    return(
+        <footer className='footer'>
+            <p>All rights reserved @startdustadventures</p>
+            <div className='social-icons-container'>
+                <p>Follow us on</p>
+                <div className='social-icons'>
+                    {SOCIAL_ICONS.map((icon, index)=>{
+                        const {name, icons : Icons} = icon
+                        return(
+                            <Icons/>
+                        )
+                    })}
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
