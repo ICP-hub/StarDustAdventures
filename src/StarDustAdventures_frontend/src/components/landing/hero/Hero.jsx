9260e3884e48bc26298407885ef1f6c7d0018442
atharav-bhatnagar-qb <atharav.bhatnagar@quadbtech.com>
2024-10-07 13:56:07 +0530

    hero animations added

--- src/StarDustAdventures_frontend/src/components/landing/hero/Hero.jsx
+++ src/StarDustAdventures_frontend/src/components/landing/hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Button from '../../re-usables/Button'
 import './hero.css'
 import BgOverlay from '../../re-usables/BgOverlay'
+import { motion } from 'framer-motion'
 
 const Hero = () => {
   return (
@@ -12,10 +13,15 @@ const Hero = () => {
                 Star Dust Adventures
             </h1>
             <div className='hstp-btn'>
-            <Button size='sm'>PLAY NOW</Button>
+            <Button size='sm'>PLAY</Button>
             </div>
         </div>
-        <div className="hs-text-cont">
+        <motion.div
+            initial={{opacity:0}}
+            animate={{opacity:1}}
+            transition={{delay:3,duration:2,type:'tween'}}
+            className="hs-text-cont"
+        >
             <h1 className="hstc-title1">
                 Star Dust 
             </h1>
@@ -25,9 +31,16 @@ const Hero = () => {
             <p className='hstc-text'>
                 A Space Exploration Tap-to-Earn Game Powered By Crypto
             </p>
-            <Button>PLAY NOW</Button>
-        </div>
-        <img src="astronaut.png" alt="falling astronaut" className="hs-falling-astro" />
+            <Button>PLAY</Button>
+        </motion.div>
+        <motion.img
+            initial={{y:-1000}}
+            animate={{y:0}}
+            transition={{duration:3,type:'tween'}}
+            src="astronaut.png" 
+            alt="falling astronaut" 
+            className="hs-falling-astro" 
+        />
         <div className="hs-text-cont2">
             <h3 className="hstc2-title">
             Overview of Star Dust Adventures
