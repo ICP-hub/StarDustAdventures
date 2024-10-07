a5c0b0e18e99a927a9f0dfee31f5db6653330e12
atharav-bhatnagar-qb <atharav.bhatnagar@quadbtech.com>
2024-10-07 13:28:51 +0530

    hero section complete(without anuimation)

--- src/StarDustAdventures_frontend/src/components/landing/hero/Hero.jsx
+++ src/StarDustAdventures_frontend/src/components/landing/hero/Hero.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Button from '../../re-usables/Button'
 import './hero.css'
+import BgOverlay from '../../re-usables/BgOverlay'
 
 const Hero = () => {
   return (
-    <div className='hero-sec'>
+    <div className='hero-sec' style={{backgroundImage:'url("hero_bg.png")',backgroundSize:'cover'}}>
+        <BgOverlay/>
         <div className="hs-top-nav">
             <h1 className="hstp-text">
                 Star Dust Adventures
@@ -14,13 +16,25 @@ const Hero = () => {
             </div>
         </div>
         <div className="hs-text-cont">
+            <h1 className="hstc-title1">
+                Star Dust 
+            </h1>
             <h1 className="hstc-title">
-                Star Dust Adventures
+            Adventures
             </h1>
             <p className='hstc-text'>
                 A Space Exploration Tap-to-Earn Game Powered By Crypto
             </p>
             <Button>PLAY NOW</Button>
+        </div>
+        <img src="astronaut.png" alt="falling astronaut" className="hs-falling-astro" />
+        <div className="hs-text-cont2">
+            <h3 className="hstc2-title">
+            Overview of Star Dust Adventures
+            </h3>
+            <p className="hstc2-text">
+            A Tap-to-Earn Game Combining Exploration and Blockchain
+            </p>
         </div>
     </div>
   )
