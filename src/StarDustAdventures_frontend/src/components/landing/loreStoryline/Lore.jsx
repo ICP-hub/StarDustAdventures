69a6669b87f8c51067a05fe78f1d216adc42b371
atharav-bhatnagar-qb <atharav.bhatnagar@quadbtech.com>
2024-10-08 16:39:29 +0530

    lore section data added

--- src/StarDustAdventures_frontend/src/components/landing/loreStoryline/Lore.jsx
+++ src/StarDustAdventures_frontend/src/components/landing/loreStoryline/Lore.jsx
@@ -1,41 +1,42 @@
 import React from 'react'
 import './lore.css'
+import { loreText } from '../../../constants/text'
 
 const Lore = () => {
   return (
     <div className='lore-sec'>
         <h1 className="ls-title">
-        Lore and Storyline
+            {loreText.title}
         </h1>
         <p className="ls-text">
-        Explore the Epic Quest for Star Dust in a Distant Future
+            {loreText.subHeading}
         </p>
         <div className="ls-rotating-astro"></div>
         <div className="ls-main">
             <div className="lsm-first">
                 <div className="lsm-text-item">
-                    <h3 className="lsm-ti-title"></h3>
-                    <div className="lsm-ti-text"></div>
+                    <h3 className="lsm-ti-title">{loreText.data[0].title}</h3>
+                    <div className="lsm-ti-text">{loreText.data[0].text}</div>
                 </div>
             </div>
             <div className="lsm-second">
                 <div className="lsm-text-item">
-                    <h3 className="lsm-ti-title"></h3>
-                    <div className="lsm-ti-text"></div>
+                    <h3 className="lsm-ti-title">{loreText.data[1].title}</h3>
+                    <div className="lsm-ti-text">{loreText.data[1].text}</div>
                 </div>
                 <div className="lsm-text-item">
-                    <h3 className="lsm-ti-title"></h3>
-                    <div className="lsm-ti-text"></div>
+                    <h3 className="lsm-ti-title">{loreText.data[2].title}</h3>
+                    <div className="lsm-ti-text">{loreText.data[2].text}</div>
                 </div>  
             </div>
             <div className="lsm-third">
                 <div className="lsm-text-item">
-                    <h3 className="lsm-ti-title"></h3>
-                    <div className="lsm-ti-text"></div>
+                    <h3 className="lsm-ti-title">{loreText.data[3].title}</h3>
+                    <div className="lsm-ti-text">{loreText.data[3].text}</div>
                 </div>
                 <div className="lsm-text-item">
-                    <h3 className="lsm-ti-title"></h3>
-                    <div className="lsm-ti-text"></div>
+                    <h3 className="lsm-ti-title">{loreText.data[4].title}</h3>
+                    <div className="lsm-ti-text">{loreText.data[4].text}</div>
                 </div>  
             </div>
         </div>
