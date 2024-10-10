5348ce986695be1bd3847a9d55fa6c3eacfdfc52
ArjunQBTech <arjun.sharma@quadbtech.com>
2024-10-10 15:48:03 +0530

    Add vite-plugin-compression to vite.config.js

--- src/StarDustAdventures_frontend/vite.config.js
+++ src/StarDustAdventures_frontend/vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'url';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import environment from 'vite-plugin-environment';
+import viteCompression from 'vite-plugin-compression';
 import dotenv from 'dotenv';
 
 dotenv.config({ path: '../../.env' });
@@ -27,6 +28,7 @@ export default defineConfig({
   },
   plugins: [
     react(),
+    viteCompression(),
     environment("all", { prefix: "CANISTER_" }),
     environment("all", { prefix: "DFX_" }),
   ],
