205f3d8cd088710f7ebaec03c0823619e138f862
MohitQuadB <mohit.sharma@quadbtech.com>
2024-10-23 15:14:33 +0530

    lore section responsive

--- src/StarDustAdventures_frontend/src/components/landing/loreStoryline/Lore.jsx
+++ src/StarDustAdventures_frontend/src/components/landing/loreStoryline/Lore.jsx
@@ -19,27 +19,27 @@ const Lore = () => {
             className="ls-rotating-astro"
         />
             <div className="lsm-first">
-                <div className="lsm-text-item">
+                <div className="lsm-text-item lsm-side-left">
                     <h3 className="lsm-ti-title">{loreText.data[0].title}</h3>
                     <div className="lsm-ti-text">{loreText.data[0].text}</div>
                 </div>
             </div>
             <div className="lsm-second">
-                <div className="lsm-text-item">
+                <div className="lsm-text-item lsm-side-right">
                     <h3 className="lsm-ti-title">{loreText.data[1].title}</h3>
                     <div className="lsm-ti-text">{loreText.data[1].text}</div>
                 </div>
-                <div className="lsm-text-item">
+                <div className="lsm-text-item  lsm-side-left">
                     <h3 className="lsm-ti-title">{loreText.data[2].title}</h3>
                     <div className="lsm-ti-text">{loreText.data[2].text}</div>
                 </div>  
             </div>
             <div className="lsm-third">
-                <div className="lsm-text-item">
+                <div className="lsm-text-item lsm-side-right">
                     <h3 className="lsm-ti-title">{loreText.data[3].title}</h3>
                     <div className="lsm-ti-text">{loreText.data[3].text}</div>
                 </div>
-                <div className="lsm-text-item">
+                <div className="lsm-text-item lsm-side-left">
                     <h3 className="lsm-ti-title">{loreText.data[4].title}</h3>
                     <div className="lsm-ti-text">{loreText.data[4].text}</div>
                 </div>  
