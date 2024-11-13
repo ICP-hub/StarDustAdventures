0dee281c0b20031da23f5c36d7f6844b378b8f50
MohitQuadB <mohit.sharma@quadbtech.com>
2024-11-13 20:01:07 +0530

    red planet drop shadow added

--- src/StarDustAdventures_frontend/src/App.jsx
+++ src/StarDustAdventures_frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import Aos from 'aos';
-import "aos/dist/aos.css";
+import "aos/dist/aos.css" ;
 
 function App() {
   useEffect(() => {
