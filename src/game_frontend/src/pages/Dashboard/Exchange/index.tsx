7ab37f2dcbeb628c5359d3d87141610c9046715b
ArjunQBTech <arjun.sharma@quadbtech.com>
2024-11-18 18:11:13 +0530

    Enhance Exchange component layout with progress bar and exchange points display

--- src/game_frontend/src/pages/Dashboard/Exchange/index.tsx
+++ src/game_frontend/src/pages/Dashboard/Exchange/index.tsx
@@ -4,10 +4,20 @@ import '../index.css'
 
 const Exchange=()=>{
     return(
-        <main>
+        <main className="exchange-container">
         <div className="progressbar-container">
             <ProgressBar/>
         </div>
+        {/* 
+        Future Scope :
+            Will be a seperate component for exchange points
+            Will fetch the data from the backend and display the points or we'll fetch the data in the page and show it then
+            Else We'll show a loading spinner till the data is fetched
+        */}
+        <section>
+            <img src='/assets/images/ufo.svg' alt="ufo" width={50} height={50}/>
+            <h3 className="exchange-points">0.3</h3>
+        </section>
         <Sidebar/>
         </main>
     )
