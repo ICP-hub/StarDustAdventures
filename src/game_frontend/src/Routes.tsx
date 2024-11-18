bbeac60716c61fc6f928694036806a11c60d2681
ArjunQBTech <arjun.sharma@quadbtech.com>
2024-11-18 17:01:55 +0530

    Refactor routing structure to include dashboard layout for exchange page

--- src/game_frontend/src/Routes.tsx
+++ src/game_frontend/src/Routes.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import Exchange from './pages/Exchange';
+import Layout from './pages/Exchange/layout';
 export default function AppRoutes() {
     return (
         <Router>
             <Routes>
                 <Route path='/' element={<Landing />} />
-                <Route path='/exchange' element={<Exchange/>} />
+                <Route path='/dashboard' element={<Layout/>} >
+                    <Route path='exchange' element={<Exchange/>} />
+                </Route>
             </Routes>
         </Router>
     )
