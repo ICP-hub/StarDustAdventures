243a6067bf4e81eac6c52c3ac50f378a9ac45e46
ArjunQBTech <arjun.sharma@quadbtech.com>
2024-11-18 16:48:33 +0530

    Add children prop rendering to SidebarTab component

--- src/game_frontend/src/components/ui/Sidebar/Tab/index.tsx
+++ src/game_frontend/src/components/ui/Sidebar/Tab/index.tsx
@@ -2,7 +2,7 @@ import  { memo } from 'react'
 const SidebarTab = memo(({ children } : SideBarChildren) => {
     return (
         <div role='tab'>
-            
+            {children}
         </div>
     )
 })
