934bc493f306d09ee9e30eb834a6d23e1b6b5d8b
ArjunQBTech <arjun.sharma@quadbtech.com>
2024-11-20 17:23:09 +0530

    Add Airdrop route to Dashboard structure

--- src/game_frontend/src/Routes.tsx
+++ src/game_frontend/src/Routes.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import Exchange from './pages/Dashboard/Exchange';
 import Layout from './pages/Dashboard/layout';
+import Airdrop from './pages/Dashboard/Airdrop';
 export default function AppRoutes() {
     return (
         <Router>
@@ -9,6 +10,7 @@ export default function AppRoutes() {
                 <Route path='/' element={<Landing />} />
                 <Route path='/dashboard' element={<Layout/>} >
                     <Route path='exchange' element={<Exchange/>} />
+                    <Route path='airdrop' element={<Airdrop/>}/>
                 </Route>
             </Routes>
         </Router>
