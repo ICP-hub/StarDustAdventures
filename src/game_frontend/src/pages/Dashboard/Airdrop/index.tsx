92f3da542c0652b799ee4466381f58a3c1f87e37
ArjunQBTech <arjun.sharma@quadbtech.com>
2024-11-20 17:24:36 +0530

    Add Airdrop component to Dashboard

---
+++ src/game_frontend/src/pages/Dashboard/Airdrop/index.tsx
@@ -0,0 +1,16 @@
+import './index.css'
+
+const Airdrop=()=>{
+    return(
+        <main className='main-container'>
+            <section>
+                <h2>We did it!</h2>
+                <div>
+                    <img src="/assets/images/ufo.svg" alt="ufo" />
+                </div>
+                <p>token is on the market</p>
+            </section>
+        </main>
+    )
+}
+export default Airdrop
