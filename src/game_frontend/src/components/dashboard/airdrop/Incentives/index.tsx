e2ed434b1670c181f4cd22993e083011e5a4c0f5
ArjunQBTech <arjun.sharma@quadbtech.com>
2024-11-20 17:24:52 +0530

    Add Incentives component to Airdrop dashboard

---
+++ src/game_frontend/src/components/dashboard/airdrop/Incentives/index.tsx
@@ -0,0 +1,58 @@
+import './index.css'
+
+const INCENTIVES_DETAILS = [
+    {
+        icon: '🚀',
+        points: 100,
+        type: 'Passive Income'
+    },
+    {
+        icon: '🎁',
+        points: 50,
+        type: 'Earn Tasks'
+    },
+    {
+        icon: '🎉',
+        points: 25,
+        type: 'Friends'
+    },
+    {
+        icon : '🚀',
+        points: 0,
+        type : 'Acheivements'
+    },
+    {
+        icon : '',
+        points : 0,
+        type : 'Keys'
+    }
+] as Array<Incentives>
+
+/**
+ * @description Incentive Card UI to show Individual Incentives
+ * @params {Incentives}
+ * @type {Functional Component}
+ */
+const IncentiveCard=({icon, points = 0, type} : Incentives)=>{
+    return(
+        <div role='group'>
+            {/* Future Scope : Use IMAGES */}
+            <p>{icon}</p>
+            <p>{type}</p>
+            <p>🛸 {points.toString()}</p>
+        </div>
+    )
+}
+
+const Incentives=()=>{
+    return(
+        <div className='incentive-group'>
+            {INCENTIVES_DETAILS.map((incentive, index)=>{
+                return(
+                    <IncentiveCard key={index} {...incentive} />
+                )
+            })}
+        </div>
+    )
+}
+export default Incentives
