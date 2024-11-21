7e737e5e7a16f83499ac9f4bfb039a8bb9cfa776
ArjunQBTech <arjun.sharma@quadbtech.com>
2024-11-21 13:20:36 +0530

    Add tabbed interface for Airdrop section with token and locals claims

--- src/game_frontend/src/pages/Dashboard/Airdrop/index.tsx
+++ src/game_frontend/src/pages/Dashboard/Airdrop/index.tsx
@@ -1,3 +1,4 @@
+import { Tab, TabPanel, Tabs } from '../../../components/ui/Tab'
 import './index.css'
 
 const Airdrop=()=>{
@@ -10,6 +11,28 @@ const Airdrop=()=>{
                 </div>
                 <p>token is on the market</p>
             </section>
+            <Tabs>
+                <Tab index={0}>
+                    <p>Tab 0</p>
+                    <TabPanel index={0}>
+                        <>
+                            <h3>Claim your tokens</h3>
+                            <p>Claim your tokens by connecting your wallet</p>
+                            <button>Connect Wallet</button>
+                        </>
+                    </TabPanel>
+                </Tab>
+                <Tab index={1}>
+                    <p>Tab 1</p>
+                    <TabPanel index={1}>
+                        <>
+                            <h3>Claim your Locals</h3>
+                            <p>Claim your tokens by connecting your wallet</p>
+                            <button>Connect Wallet</button>
+                        </>
+                    </TabPanel>
+                </Tab>
+            </Tabs>
         </main>
     )
 }
