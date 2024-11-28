3bbda23d4c17f438612929794e04bcb37f536c0f
ArjunQBTech <arjun.sharma@quadbtech.com>
2024-11-28 13:49:24 +0530

    Add routes for Invite Friends and Rewards List pages

--- src/game_frontend/src/Routes.tsx
+++ src/game_frontend/src/Routes.tsx
@@ -3,14 +3,18 @@ import Landing from './pages/Landing';
 import Exchange from './pages/Dashboard/Exchange';
 import Layout from './pages/Dashboard/layout';
 import Airdrop from './pages/Dashboard/Airdrop';
+import InviteFriends from './pages/Dashboard/InviteFriends';
+import RewardsList from './pages/Dashboard/RewardsList';
 export default function AppRoutes() {
     return (
         <Router>
             <Routes>
                 <Route path='/' element={<Landing />} />
                 <Route path='/dashboard' element={<Layout/>} >
-                    <Route path='exchange' element={<Exchange/>} />
+                    <Route index element={<Exchange/>} />
                     <Route path='airdrop' element={<Airdrop/>}/>
+                    <Route path='invite-friends' element={<InviteFriends/>}/>
+                    <Route path='rewards' element={<RewardsList/>}/>
                 </Route>
             </Routes>
         </Router>
