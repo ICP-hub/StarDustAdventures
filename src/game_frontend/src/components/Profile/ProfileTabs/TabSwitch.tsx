4933f37ed348922bbeca08cbcf53057b41999576
Shadab16 <shadab.khan@quadbtech.com>
2024-11-30 16:12:32 +0530

    Refactor: move file to new location and add responsiveness

--- src/game_frontend/src/components/Profile/ProfileTabs/TabSwitch.tsx
+++ src/game_frontend/src/components/Profile/ProfileTabs/TabSwitch.tsx
@@ -1,44 +1,53 @@
-import React from 'react';
-import { Tabs, Tab, TabPanel } from '../../ui/Tab';
-import { AchievementsDataTab, SettingDataTab } from './TabSwitchData';
+import React from "react";
+import { Tabs, Tab, TabPanel } from "../../ui/Tab";
+import { AchievementsDataTab, SettingDataTab } from "./TabSwitchData";
 
 interface Achievement {
-    id: number;
-    title: string;
+  id: number;
+  title: string;
 }
 
 interface Setting {
-    id: number;
-    title: string;
-    language: string;
+  id: number;
+  title: string;
+  language: string;
 }
 
 interface TabSwitchProps {
-    achievementsData: Achievement[];
-    settingsData: Setting[];
+  achievementsData: Achievement[];
+  settingsData: Setting[];
 }
 
-const TabSwitch: React.FC<TabSwitchProps> = ({ achievementsData, settingsData }) => {
-    return (
-        <div className="h-screen w-full">
-           <div className='flex justify-center mt-2'>
-           <div className="h-full w-4/5 ">
-                <Tabs>
-                    <Tab index={0}><p>Achievement</p></Tab>
-                    <Tab index={1}><p>Setting</p></Tab>
-
-                    <TabPanel index={0}>
-                        <AchievementsDataTab achievementsData={achievementsData} />
-                    </TabPanel>
-
-                    <TabPanel index={1}>
-                        <SettingDataTab settingsData={settingsData} />
-                    </TabPanel>
-                </Tabs>
+const TabSwitch: React.FC<TabSwitchProps> = ({
+  achievementsData,
+  settingsData,
+}) => {
+  return (
+    <div className="h-screen w-full">
+      <div className="flex justify-center mt-2">
+        <div className="h-full ">
+          <Tabs>
+            <div className="flex">
+              <Tab index={0}>
+                <p>Achievement</p>
+              </Tab>
+              <Tab index={1}>
+                <p>Setting</p>
+              </Tab>
             </div>
-           </div>
+
+            <TabPanel index={0}>
+              <AchievementsDataTab achievementsData={achievementsData} />
+            </TabPanel>
+
+            <TabPanel index={1}>
+              <SettingDataTab settingsData={settingsData} />
+            </TabPanel>
+          </Tabs>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default TabSwitch;
