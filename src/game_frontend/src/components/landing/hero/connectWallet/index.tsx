7969047d4d00636c1df7fb0557633775b8274322
ArjunQBTech <arjun.sharma@quadbtech.com>
2024-12-02 11:41:03 +0530

    Implement user registration on successful wallet login in ConnectWallet component

--- src/game_frontend/src/components/landing/hero/connectWallet/index.tsx
+++ src/game_frontend/src/components/landing/hero/connectWallet/index.tsx
@@ -3,6 +3,8 @@ import Modal from "../../../ui/Modal";
 import { ModalHeader, ModalTitle, ModalCloseBtn, ModalBody } from "../../../ui/Modal/utils";
 import { useAuth } from "../../../../hooks/useAuth";
 import './index.css';
+import { CREATE_USER } from "../../../../utils/api/update";
+import { useNavigate } from "react-router-dom";
 
 const Wallets : WalletType[] = [
     {
@@ -29,6 +31,39 @@ const Wallets : WalletType[] = [
 
 export default function ConnectWallet({closeModal} : {closeModal:()=>void}) {
     const auth = useAuth();
+    const navigate = useNavigate()
+    
+    const {mutateAsync, data} = CREATE_USER(auth?.actors, {
+        user:{name:"Test"}, 
+        refBy:[]
+    });
+
+    console.log("Data : ", data)
+    // Temporary Function : Register User on Successfull Login
+    const handleLogin = async (method: "ii" | "plug") => {
+        if (!auth) {
+            console.error("Auth context is not available.");
+            return;
+        }
+    
+        try {
+            const loginStatus = await auth.login(method);
+            if(loginStatus) {
+                const response : any = await mutateAsync();
+                if(response.ok){
+                    console.log("User Registered Successfully");
+                    closeModal();
+                }
+                navigate('/dashboard');
+            }
+            
+        } catch (error) {
+            console.error("Login failed:", error);
+            // Handle user feedback on error
+        }
+    };
+    
+
     return (
         <Modal onClose={closeModal}>
             <ModalHeader>
@@ -39,7 +74,7 @@ export default function ConnectWallet({closeModal} : {closeModal:()=>void}) {
                 <div className="flex flex-col gap-4 w-full lg:py-6">
                     {
                         Wallets.map(wallet => (
-                            <Button variant="secondary" key={wallet.name} className="wallet-button" onClick={()=>auth?.login(wallet.method)}>
+                            <Button variant="secondary" key={wallet.name} className="wallet-button" onClick={()=>handleLogin(wallet.method)}>
                                 <div className="wallet-button-container">
                                     <span>{wallet.name}</span>
                                     <img src={wallet.icon} alt={wallet.name} loading='lazy' width={40} height={40} className="wallet-icon"/>
