250c2ba50c53c9691e52a0c2d7573ac2c78ca0fe
ArjunQBTech <arjun.sharma@quadbtech.com>
2024-12-03 13:57:48 +0530

    Refactor Exchange component to integrate user points fetching and loading state

--- src/game_frontend/src/pages/Dashboard/Exchange/index.tsx
+++ src/game_frontend/src/pages/Dashboard/Exchange/index.tsx
@@ -1,18 +1,23 @@
-import { useLoaderData } from "react-router-dom"
 import ProgressBar from "../../../components/ui/Progressbar"
 import Sidebar from "../../../components/ui/Sidebar"
-import { GET_USER_POINTS } from "../../../utils/api/query"
 import '../index.css'
 import { useEffect, useState } from "react"
 import { bigIntToNumber } from "../../../utils"
 import usePoints from "../../../hooks/usePoints"
+import { GET_USER_POINTS } from "../../../utils/api/query"
+import { useAuth } from "../../../hooks/useAuth"
 
 const Exchange=()=>{
-    const data : any = useLoaderData()
+    const auth = useAuth()
+    const {data , isLoading} : {data : any, isLoading : boolean} = GET_USER_POINTS(auth?.actors)
     const {points, setPoints, incrementPoints} = usePoints(0)
+    // Future 
+    /**
+     * Batch Update after a set of interval
+     */
 
     useEffect(()=>{
-        if(data.ok){
+        if(data?.ok){
             const _points = bigIntToNumber(data.ok) * 0.001
             setPoints(_points)
         }
@@ -20,34 +25,24 @@ const Exchange=()=>{
 
     return(
         <main className="exchange-container">
+       {isLoading ? <p>Loading...</p>: (
+        <>
         <div className="progressbar-container">
             <ProgressBar/>
         </div>
-        {/* 
-        Future Scope :
-            Will be a seperate component for exchange points
-            Will fetch the data from the backend and display the points or we'll fetch the data in the page and show it then
-            Else We'll show a loading spinner till the data is fetched
-        */}
         <section>
             <img src='/assets/images/ufo.svg' alt="ufo" width={50} height={50} loading="lazy"/>
             <h3 className="exchange-points">{points.toString()}</h3>
         </section>
         <Sidebar/>
         <section
-        onClick={()=>incrementPoints}
+        onClick={incrementPoints}
         className="click-to-earn" style={{backgroundImage: 'url("/assets/images/firefly.webp")'}}>
         </section>
+    </>    
+    )
+        }
         </main>
     )
 }
-
-export const ExchangeLoader=async(_: any, actors: any)=>{
-    if (!actors) {
-        return null;
-    }
-    const { data, error } = GET_USER_POINTS(actors); // Fetch user data
-    return data as {ok : Number; err : any};
-}
-
 export default Exchange
