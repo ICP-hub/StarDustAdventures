7fdc74e7cbd7200e7e723867df3a73e63f790049
ArjunQBTech <arjun.sharma@quadbtech.com>
2024-12-09 15:49:41 +0530

    Refactor useFriendList hook to simplify friend list type and update state handling

--- src/game_frontend/src/hooks/useFriendList.tsx
+++ src/game_frontend/src/hooks/useFriendList.tsx
@@ -3,19 +3,16 @@ import { GET_USER_FRIENDS } from "../utils/api/query";
 import { useAuth } from "./useAuth";
 import { Principal } from "@dfinity/principal";
 
-type useFriendType = {
-    principal : Principal,
-    name : string,
-}
+type useFriendType = Array<[Principal, string]>
 
 const useFriendList = ()=>{
     const auth = useAuth()
-    const [friendList, setFriendList] = useState<Array<useFriendType> | undefined>(undefined);
+    const [friendList, setFriendList] = useState<useFriendType | undefined>(undefined);
     const {data, isLoading, error, refetch, isRefetching} = GET_USER_FRIENDS(auth?.actors)
 
     useEffect(()=>{
         if((data as any)?.ok){
-            setFriendList((data as any)?.ok)
+            setFriendList((data as any)?.ok[0])
         }
     },[data])
 
