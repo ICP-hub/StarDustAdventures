1b5529c4200859c558e4682b141c71cde4642f0f
ArjunQBTech <arjun.sharma@quadbtech.com>
2024-12-12 14:58:56 +0530

    Refactor routing to use lazy loading for dashboard components and wrap Layout in ProtectedRoutes

--- src/game_frontend/src/Routes.tsx
+++ src/game_frontend/src/Routes.tsx
@@ -1,13 +1,23 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Landing from './pages/Landing';
-import Exchange from './pages/Dashboard/Exchange';
-import Layout from './pages/Dashboard/layout';
-import Airdrop from './pages/Dashboard/Airdrop';
-import InviteFriends from './pages/Dashboard/InviteFriends';
-import RewardsList from './pages/Dashboard/RewardsList';
-import MineCard from './pages/Dashboard/MineCard';
-import ProfileTabs from './pages/Dashboard/ProfilePage';
+// import Exchange from './pages/Dashboard/Exchange';
+// import Layout from './pages/Dashboard/layout';
+// import Airdrop from './pages/Dashboard/Airdrop';
+// import InviteFriends from './pages/Dashboard/InviteFriends';
+// import RewardsList from './pages/Dashboard/RewardsList';
+// import MineCard from './pages/Dashboard/MineCard';
+// import ProfileTabs from './pages/Dashboard/ProfilePage';
 import RegisterScreen from './pages/Register';
+import { lazy } from 'react';
+import ProtectedRoutes from './components/ProtectedRoutes';
+
+const Layout = lazy(()=>import('./pages/Dashboard/layout'))
+const Exchange = lazy(()=>import('./pages/Dashboard/Exchange'))
+const Airdrop = lazy(()=>import('./pages/Dashboard/Airdrop'))
+const InviteFriends = lazy(()=>import('./pages/Dashboard/InviteFriends'))
+const RewardsList = lazy(()=>import('./pages/Dashboard/RewardsList'))
+const MineCard = lazy(()=>import('./pages/Dashboard/MineCard'))
+const ProfileTabs = lazy(()=>import('./pages/Dashboard/ProfilePage'))
 
 
 export default function AppRoutes() {
@@ -22,11 +32,15 @@ export default function AppRoutes() {
         },
         {
             path: '/dashboard',
-            element: <Layout />,
+            element: (
+            <ProtectedRoutes>
+                <Layout />
+            </ProtectedRoutes>
+        ),
             children: [
                 {
                     index: true,
-                    element: <Exchange />,
+                    element: <Exchange/>,
                 },
                 {
                     path: 'exchange',
